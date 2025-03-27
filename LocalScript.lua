b3a929214d9bb6df4b50a40b9dfa2c1b0c8a212f
Roth <82582846+RothGorham@users.noreply.github.com>
2025-03-27 11:44:45 -0300

    arquivo novo

--- LocalScript.lua
+++ LocalScript.lua
@@ -5,15 +5,13 @@ local remote = ReplicatedStorage:WaitForChild("NotificarJogador")
 local ultimaNotificacaoId = nil
 
 remote.OnClientEvent:Connect(function(titulo, mensagem)
-	-- Não mostrar "Ocultar"
 	if titulo == "Ocultar" then return end
 
 	local id = HttpService:GenerateGUID(false)
 	ultimaNotificacaoId = id
 
-	local duracao = 60 -- tempo padrão
+	local duracao = 60
 
-	-- Se for "Resultado", mostrar por 2 segundos e cancelar
 	if titulo == "Resultado" then
 		duracao = 10
 		game.StarterGui:SetCore("SendNotification", {
@@ -28,9 +26,7 @@ remote.OnClientEvent:Connect(function(titulo, mensagem)
 				game.StarterGui:SetCore("CancelNotification", id)
 			end)
 		end)
-
 	else
-		-- Mostra pergunta ou aguardando e mantém por 60s
 		game.StarterGui:SetCore("SendNotification", {
 			Title = titulo,
 			Text = mensagem,
